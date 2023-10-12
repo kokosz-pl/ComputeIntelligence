4cbee642744b2159207fc0b0e1b7cc6d661a3df1
Michal Kokosza <kokosza.michal@gmail.com>
2023-10-12 18:03:51 +0200

    Lab nr 1

--- lab1.py
+++ lab1.py
@@ -31,46 +31,7 @@ def standardize_vec(x):
     return z
 
 
-if __name__ == '__main__':
-
-    # # Prime Nums
-
-    # arr = [3, 6, 11, 25, 19]
-    # print(f"Prime numbers: {select_prime(arr)}")
-
-    # # Vectors
-
-    # arr1 = np.array([3, 8, 9, 10, 12])
-    # arr2 = np.array([8, 7, 7, 5, 6])
-
-    # arr_sum = arr1 + arr2
-    # arr_mul = arr1 * arr2
-
-    # print(f"Vector Sum: {arr_sum}")
-    # print(f"Vector Product: {arr_mul}")
-
-    # dot_product = np.dot(arr1, arr2)
-    # euclidan_dist = np.sqrt(np.power(arr1, 2) + np.power(arr2, 2))
-
-    # print(f"Vector Dot Product: {dot_product}")
-
-    # print(f"Euclidan Distance: {euclidan_dist}")
-
-    # int_arr = np.random.randint(1, 100, 50)
-
-    # print(f"Max value: {max(int_arr)}, Min value: {min(int_arr)}")
-    # print(f"Average value: {np.average(int_arr)}")
-    # print(f"Standard Deviation: {np.std(int_arr)}")
-
-    # normalized_arr = normalize_vec(int_arr)
-
-    # print(f"Before normaliztion: value {max(int_arr)}, index {np.argmax(int_arr)}")
-    # print(f"After normaliztion: value {max(normalized_arr)}, index {np.argmax(normalized_arr)}")
-
-    # standardized_arr = standardize_vec(int_arr)
-    # print(f"Standardized vector: avg {np.average(standardized_arr)}, deviation {np.std(standardized_arr)}")
-
-    # plot
+def plot_data():
 
     miasta = pd.read_csv("./miasta.csv")
     miasta.loc[len(miasta)] = [2010, 460, 555, 405]
@@ -86,4 +47,46 @@ if __name__ == '__main__':
     plt.title("Ludnosc w miastach Polski")
     plt.legend()
     plt.show()
-    # print(miasta.values[:, 1])
+
+
+if __name__ == '__main__':
+
+    # Prime Nums
+
+    arr = [3, 6, 11, 25, 19]
+    print(f"Prime numbers: {select_prime(arr)}")
+
+    # Vectors
+
+    arr1 = np.array([3, 8, 9, 10, 12])
+    arr2 = np.array([8, 7, 7, 5, 6])
+
+    arr_sum = arr1 + arr2
+    arr_mul = arr1 * arr2
+
+    print(f"Vector Sum: {arr_sum}")
+    print(f"Vector Product: {arr_mul}")
+
+    dot_product = np.dot(arr1, arr2)
+    euclidan_dist = np.sqrt(np.power(arr1, 2) + np.power(arr2, 2))
+
+    print(f"Vector Dot Product: {dot_product}")
+
+    print(f"Euclidan Distance: {euclidan_dist}")
+
+    int_arr = np.random.randint(1, 100, 50)
+
+    print(f"Max value: {max(int_arr)}, Min value: {min(int_arr)}")
+    print(f"Average value: {np.average(int_arr)}")
+    print(f"Standard Deviation: {np.std(int_arr)}")
+
+    normalized_arr = normalize_vec(int_arr)
+
+    print(f"Before normaliztion: value {max(int_arr)}, index {np.argmax(int_arr)}")
+    print(f"After normaliztion: value {max(normalized_arr)}, index {np.argmax(normalized_arr)}")
+
+    standardized_arr = standardize_vec(int_arr)
+    print(f"Standardized vector: avg {np.average(standardized_arr)}, deviation {np.std(standardized_arr)}")
+
+    # plot
+    plot_data()
